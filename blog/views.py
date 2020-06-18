ee61c084f5166988d0fc101fb3e8c578dbd6cbc3
Your Name <you@youraddress.com>
2020-06-18 11:22:25 +0300

    добавлен поиск

--- blog/views.py
+++ blog/views.py
@@ -66,6 +66,25 @@ class PostByTag(ListView):
         context['title'] = 'Записи по тегу' + str(Tag.objects.get(slug=self.kwargs['slug']))
         return context
 
+
+class Search(ListView):
+    template_name = 'blog/search.html'
+    context_obj_name = 'posts'
+    paginate_by = 1
+
+
+    def get_queryset(self):
+        return Post.objects.filter(title__icontains=self.request.GET.get('s'))
+
+    def get_context_data(self,*args,**kwargs):
+        context = super().get_context_data(*args,**kwargs)
+        #context['s']  = f"s={self.request.GET.get('s')}&"
+        return context
+
+
+
+
+
 def index(request):
     template = 'blog/index.html'
     context = {}
